e6b1eb0f61ca9ed3e3251034f79c0b4d5a2da396
mostafa <desham8@gmail.com>
2024-10-05 21:10:50 +0300

    pascal's triangle

---
+++ 0-pascal_triangle.py
@@ -0,0 +1,14 @@
+def pascal_triangle(n):
+    triangle = []
+
+    if n <= 0:
+        return []
+
+    for r in range(n):
+        coef = 1
+        row = []
+        for c in range(r + 1):
+            row.append(coef)
+            coef = coef * (r - c) // (c + 1)
+        triangle.append(row)
+    return triangle
